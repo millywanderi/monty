0483d8c5a5717b12484417f36c7c24288853b811
millywanderi <milliwanderi5@gmail.com>
2023-05-27 14:32:37 -0700

    added print file error

--- handle_err.c
+++ handle_err.c
@@ -10,3 +10,13 @@ void print_err(char *argv)
 	fprintf(stderr, "Error: Can't open file %s\n", argv);
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * print_file_err - prints file error message
+ * Return: void
+ */
+void print_file_err(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+}
