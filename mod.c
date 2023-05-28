3b0fb3936dff8861d1e53242ce36f077d3397222
millywanderi <milliwanderi5@gmail.com>
2023-05-28 03:56:31 -0700

    modulus of two

---
+++ mod.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * mod - finds if a element is divisible by two
+ * @sta: stack
+ * @line_num: line number
+ * Return: void
+ */
+void mod(stack_t **sta, unsigned int line_num)
+{
+	int module;
+
+	if (sta == NULL || *sta == NULL || ((*sta)->next) == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_num);
+	exit(EXIT_FAILURE);
+	}
+	else if (((*sta)->n) == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	module = ((*sta)->next->n) % ((*sta)->n);
+	pop(sta, line_num);
+	(*sta)->n = module;
+}
