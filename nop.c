04b98bdaea9347a096e64a09ddc5a25d95024a50
millywanderi <milliwanderi5@gmail.com>
2023-05-28 03:08:34 -0700

    does nothing

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - a function that does nothing
+ * @sta: stack
+ * @line_num: line number
+ * Return: void
+ */
+void nop(stack_t **sta, unsigned int line_num)
+{
+	(void) line_num;
+	(void) sta;
+}
