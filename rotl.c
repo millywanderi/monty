db20fdaeffa36d5fc96674dc3bc618d5c9f4657e
millywanderi <milliwanderi5@gmail.com>
2023-05-28 04:48:27 -0700

    rrotates the stack elements

---
+++ rotl.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * rotl - rotates elements
+ * @sta: stack
+ * @line_num: line number
+ * Return: void
+ */
+void rotl(stack_t **sta, unsigned int line_num)
+{
+	(void) line_num;
+	stack_t *up;
+	stack_t *dow;
+
+	if (!sta || !*sta || !(*sta)->next)
+	{
+		return;
+	}
+	dow = up = *sta;
+	while (up->next)
+	{
+		up = up->next;
+	}
+	up->next = dow;
+	dow->prev = up;
+	*sta = dow->next;
+	(*sta)->prev->next = NULL;
+	(*sta)->prev = NULL;
+}
