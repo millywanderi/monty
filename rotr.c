ba229fb6dc34ab54a42baf0589ee7c45705777a1
millywanderi <milliwanderi5@gmail.com>
2023-05-28 05:07:36 -0700

    rotates the stack to the bottom

---
+++ rotr.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * rotr - rotates the stack to the bottom
+ * @sta: stack
+ * @line_num: line number
+ * Return: void
+ */
+void rotr(stack_t **sta, unsigned int line_num)
+{
+	(void) line_num;
+	stack_t *up;
+	stack_t *dow;
+
+	if (!sta || !*sta || !(*sta)->next)
+	{
+		return;
+	}
+	dow = *sta;
+	while (dow->next)
+	{
+		dow = dow->next;
+	}
+	up = dow->prev;
+	dow->next = *sta;
+	dow->next = NULL;
+	up->next = NULL;
+	(*sta)->prev = dow;
+	*sta = dow;
+}
